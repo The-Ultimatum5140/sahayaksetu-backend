f116b89a4f420cfc30038f2df69a48b9ae7862e2
Dipu Gupta <deepukusaura@gmail.com>
2026-05-06 02:15:12 +0530

    Done

--- server.js
+++ server.js
@@ -20,7 +20,22 @@ connectCloudinary();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+const allowedOrigins = process.env.CLIENT_URL.split(",");
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 
 // initializing end points
